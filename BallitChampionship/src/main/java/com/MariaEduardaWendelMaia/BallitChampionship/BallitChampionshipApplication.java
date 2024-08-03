fd6773864439ff401891742386af0f63e749377b
Maria Maia <113213701+DudaWendelMaia@users.noreply.github.com>
2024-08-03 19:31:44 -0300

    Add files via upload

---
+++ BallitChampionship/src/main/java/com/MariaEduardaWendelMaia/BallitChampionship/BallitChampionshipApplication.java
@@ -0,0 +1,13 @@
+package com.MariaEduardaWendelMaia.BallitChampionship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BallitChampionshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BallitChampionshipApplication.class, args);
+	}
+
+}
---
+++ BallitChampionship/src/test/java/com/MariaEduardaWendelMaia/BallitChampionship/BallitChampionshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MariaEduardaWendelMaia.BallitChampionship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BallitChampionshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
